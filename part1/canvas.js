d70b813a0451fcdf98cd22f2fc35a777f5852283
Harry <kidflash7011@gmail.com>
2023-12-26 13:41:51 +0800

    mouse track complete

--- part1/canvas.js
+++ part1/canvas.js
@@ -1,60 +1,127 @@
 // canvas.js
-// Manages the tracer canvas for tracking finger movement
+// This javascript is used to manage the tracer canvas 
 
-$(document).ready(function() {
-    // Initialize variables and set starting/ending points for the path
-    var startPoint = { x: 100, y: 200 };
-    var endPoint = { x: 700, y: startPoint.y };
-    var hoverTargetsRadius = 20;
-    var pathPoints = [];
-    var isTracking = false;
-
-    // Setup the user path layer and draw the guideline
-    addUserPathLayer();
-    drawGuideLine();
+    $(document).ready(function() {
+        // 設定兩個圓的位置
+        var startPoint = { x: 100, y: 200 };
+        var endPoint = { x: 700, y: startPoint.y };
+        // 圓半徑
+        var hoverTargetsRadius = 20;
+        // 用於儲存用戶的路徑
+        var pathPoints = [];
+        var isTracking = false;
     
-    // Draw starting and target circles
-    drawStartingCircle();
-    drawTargetCircle();
-
-    // Add text instructions and setup cursor tracking
-    addTextInstructions();
-    setupCursorTracking();
-
-    // Functions used in the program
-    function addUserPathLayer() {
-        // Creates a layer for the red user path
-    }
-
-    function drawGuideLine() {
-        // Draws a grey guide line on the canvas
-    }
-
-    function drawStartingCircle() {
-        // Draws the starting green circle and handles mouse events
-    }
-
-    function drawTargetCircle() {
-        // Draws the target blue circle and handles mouse events
-    }
-
-    function addTextInstructions() {
-        // Adds text instructions to the top of the canvas
-    }
-
-    function setupCursorTracking() {
-        // Sets up tracking of cursor movement and stores path points
-    }
-
-    function resetPath() {
-        // Clears the path points and resets tracking
-    }
-
-    function showGuideline(show) {
-        // Shows or hides the guideline based on the given parameter
-    }
-
-    function analyzePerformance() {
-        // Calculates and displays the average deviation of the user path
-    }
-});
+        addUserPathLayer();
+    
+        // 灰色線段
+        $('canvas').drawLine({
+            x1: startPoint.x + hoverTargetsRadius,
+            y1: startPoint.y,
+            x2: endPoint.x - hoverTargetsRadius,
+            y2: endPoint.y,
+            layer: true,
+            strokeWidth: 3,
+            strokeStyle: '#aaa',
+            visible: false,
+            name: 'guideline'
+        });
+    
+        $('canvas').drawArc({
+            fillStyle: '#0a0',
+            x: startPoint.x,
+            y: startPoint.y,
+            radius: hoverTargetsRadius,
+            layer: true,
+            name: 'startCircle',
+            mouseover: function() {
+                $(this).animateLayer('startCircle', {
+                    fillStyle: '#0c0'
+                }, 250);
+                resetPath();
+                isTracking = true;
+                showGuideline(false);
+            },
+            mouseout: function() {
+                $(this).animateLayer('startCircle', {
+                    fillStyle: '#0a0'
+                }, 250);
+            }
+        });
+    
+        $('canvas').drawArc({
+            fillStyle: '#00d',
+            x: endPoint.x,
+            y: endPoint.y,
+            radius: hoverTargetsRadius,
+            layer: true,
+            name: 'targetCircle',
+            mouseover: function() {
+                $(this).animateLayer('targetCircle', {
+                    fillStyle: '#55f'
+                }, 250);
+                if (isTracking) {
+                    isTracking = false;
+                    showGuideline(true);
+                    analyzePerformance();
+                }
+            },
+            mouseout: function() {
+                $(this).animateLayer('targetCircle', {
+                    fillStyle: '#00d'
+                }, 250);
+            }
+        });
+    
+        $('canvas').drawText({
+            fillStyle: '#000',
+            x: 400, y: 20,
+            fontSize: 16,
+            text: 'Mouseover the green circle and draw a straight line to the blue circle.',
+            layer: true,
+            name: 'resetText'
+        });
+    
+        $('canvas').mousemove(function(event) {
+            if (isTracking) {
+                var rect = $('canvas').offset();
+                var cursor_x = event.pageX - rect.left;
+                var cursor_y = event.pageY - rect.top;
+                pathPoints.push([cursor_x, cursor_y]);
+    
+                var i = pathPoints.length;
+                var pathLayer = $('canvas').getLayer('userPath');
+                pathLayer['x' + i] = pathPoints[i - 1][0];
+                pathLayer['y' + i] = pathPoints[i - 1][1];
+            }
+        });
+    
+        function addUserPathLayer() {
+            $('canvas').addLayer({
+                name: 'userPath',
+                type: 'line',
+                strokeStyle: '#f00',
+                strokeWidth: 3
+            });
+        }
+    
+        function resetPath() {
+            pathPoints = [];
+            isTracking = false;
+            $('canvas').removeLayer('userPath');
+            addUserPathLayer();
+        }
+    
+        function showGuideline(show) {
+            $('canvas').setLayer('guideline', { visible: show }).drawLayers();
+        }
+    
+        function analyzePerformance() {
+            var sumOfDeviations = 0;
+            for (var i = 0; i < pathPoints.length; i++) {
+                var deviationY = pathPoints[i][1] - startPoint.y;
+                sumOfDeviations += Math.abs(deviationY);
+            }
+            $('#results').html('Average Deviation: ' + Math.round(sumOfDeviations / pathPoints.length) + ' pixels');
+        }
+    });
+    
